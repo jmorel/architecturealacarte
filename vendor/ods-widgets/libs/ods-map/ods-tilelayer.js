212cdcb453ecf05ecfb1d4d9946545bb581d2495
Jeremy Morel <jeremy.morel@opendatasoft.com>
2015-08-23 19:04:35 +0200

    actual style for desktop

---
+++ vendor/ods-widgets/libs/ods-map/ods-tilelayer.js
@@ -0,0 +1,150 @@
+var ODSTileLayerMixin = {
+    odsOptions: {
+        basemap: null,
+        appendAttribution: null,
+        prependAttribution: null,
+        disableAttribution: null,
+        attributionSeparator: ' - '
+    },
+    _addAttributionPart: function(attribution, part) {
+        if (part) {
+            if (attribution) {
+                attribution += this.options.attributionSeparator;
+            }
+            attribution += part;
+        }
+        return attribution;
+    }
+};
+
+L.ODSTileLayer = L.TileLayer.extend({
+    includes: ODSTileLayerMixin,
+    initialize: function(options) {
+        L.Util.setOptions(this, this.odsOptions);
+        L.Util.setOptions(this, options);
+        this._initLayer(
+            this.options.basemap,
+            this.options.disableAttribution,
+            this.options.prependAttribution,
+            this.options.appendAttribution);
+    },
+    _mapboxUrl: function(mapId, accessToken) {
+        var url = '//{s}.tiles.mapbox.com/v4/' + mapId + '/{z}/{x}/{y}';
+        if (L.Browser.retina) {
+            url += '@2x';
+        }
+        url += '.png';
+        url += '?access_token=' + accessToken;
+        return url;
+    },
+    _initLayer: function(basemap, disableAttribution, prependAttribution, appendAttribution) {
+        var layerOptions = {};
+        var attrib = this._addAttributionPart('', prependAttribution);
+        if (basemap.provider === 'mapquest') {
+            // OSM MapQuest
+            attrib = this._addAttributionPart(attrib, 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"> - Map data © <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors');
+            attrib = this._addAttributionPart(attrib, appendAttribution);
+            L.TileLayer.prototype.initialize.call(this, 'http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png',
+                {
+                    minZoom: 1,
+                    maxNativeZoom: 18,
+                    maxZoom: 19,
+                    attribution: !disableAttribution ? attrib : '',
+                    subdomains: "1234"
+                });
+        } else if (basemap.provider === 'opencycle') {
+            attrib = this._addAttributionPart(attrib, 'Tiles Courtesy of <a href="http://www.thunderforest.com" target="_blank">Thunderforest</a> - Map data © <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors');
+            attrib = this._addAttributionPart(attrib, appendAttribution);
+            L.TileLayer.prototype.initialize.call(this, 'http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png',
+                {
+                    minZoom: 1,
+                    maxNativeZoom: 18,
+                    maxZoom: 19,
+                    attribution: !disableAttribution ? attrib : '',
+                    subdomains: "abc"
+                });
+        } else if (basemap.provider === 'osmtransport') {
+            attrib = this._addAttributionPart(attrib, 'Tiles Courtesy of <a href="http://www.thunderforest.com" target="_blank">Thunderforest</a> - Map data © <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors');
+            attrib = this._addAttributionPart(attrib, appendAttribution);
+            L.TileLayer.prototype.initialize.call(this, 'http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png',
+                {
+                    minZoom: 1,
+                    maxNativeZoom: 18,
+                    maxZoom: 19,
+                    attribution: !disableAttribution ? attrib : '',
+                    subdomains: "abc"
+                });
+        } else if (basemap.provider.indexOf('mapbox.') === 0) {
+            attrib = this._addAttributionPart(attrib, 'Map data © <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors');
+            attrib = this._addAttributionPart(attrib, appendAttribution);
+            layerOptions = {
+                minZoom: 1,
+                maxZoom: 21,
+                attribution: !disableAttribution ? attrib : '',
+                subdomains: "abcd"
+            };
+            L.TileLayer.prototype.initialize.call(this, this._mapboxUrl(basemap.provider, basemap.mapbox_access_token), layerOptions);
+
+        } else if (basemap.provider === 'mapbox') {
+            attrib = this._addAttributionPart(attrib, 'Map data © <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors');
+            attrib = this._addAttributionPart(attrib, appendAttribution);
+            layerOptions = {
+                minZoom: 1,
+                maxZoom: 21,
+                attribution: !disableAttribution ? attrib : '',
+                subdomains: "abcd"
+            };
+            if (basemap.minZoom) {
+                layerOptions.minZoom = basemap.minZoom;
+            }
+            if (basemap.maxZoom) {
+                layerOptions.maxZoom = basemap.maxZoom;
+            }
+            L.TileLayer.prototype.initialize.call(this, this._mapboxUrl(basemap.mapid, basemap.mapbox_access_token), layerOptions);
+        } else if (basemap.provider === 'custom') {
+            if (basemap.subdomains) {
+                layerOptions.subdomains = basemap.subdomains;
+            }
+            if (basemap.minZoom) {
+                layerOptions.minZoom = basemap.minZoom;
+            }
+            if (basemap.maxZoom) {
+                layerOptions.maxZoom = basemap.maxZoom;
+            }
+
+            attrib = this._addAttributionPart(attrib, basemap.attribution);
+            attrib = this._addAttributionPart(attrib, appendAttribution);
+
+            layerOptions.attribution = !disableAttribution ? attrib : '';
+            L.TileLayer.prototype.initialize.call(this, basemap.url, layerOptions);
+        }
+    }
+});
+
+L.ODSWMSTileLayer = L.TileLayer.WMS.extend({
+    includes: ODSTileLayerMixin,
+    initialize: function(options) {
+        L.Util.setOptions(this, this.odsOptions);
+        L.Util.setOptions(this, options);
+        this._initLayer(
+            this.options.basemap,
+            this.options.disableAttribution,
+            this.options.prependAttribution,
+            this.options.appendAttribution);
+    },
+    _initLayer: function(basemap, disableAttribution, prependAttribution, appendAttribution) {
+        var layerOptions = {};
+        var attrib = this._addAttributionPart('', prependAttribution);
+
+        layerOptions.layers = basemap.layers;
+        if (basemap.styles) {
+            layerOptions.styles = basemap.styles;
+        }
+
+        attrib = this._addAttributionPart(attrib, basemap.attribution);
+        attrib = this._addAttributionPart(attrib, appendAttribution);
+
+        layerOptions.attribution = !disableAttribution ? attrib : '';
+        L.TileLayer.WMS.prototype.initialize.call(this, basemap.url, layerOptions);
+    }
+});
---
+++ vendor/ods-widgets/libs/ods-vectormarker/vectormarker.js
@@ -0,0 +1,53 @@
+L.VectorMarker = L.Marker.extend({
+    options: {
+        color: '#FF0000',
+        icon: null,
+        marker: true
+    },
+    initialize: function(latlng, options) {
+        L.Util.setOptions(this, options);
+        // Create divicon
+        if (Modernizr.inlinesvg) {
+            this.options.icon = this._getVectorIcon(this.options.color, this.options.icon, this.options.marker);
+        } else {
+            this.options.icon = new L.Icon.Default();
+        }
+        L.Marker.prototype.initialize.call(this, latlng, this.options);
+    },
+    _isSVGIcon: function() {
+        return !(typeof this.options.icon == 'string' || this.options.icon instanceof String);
+    },
+    _getVectorIcon: function(color, icon, marker) {
+
+        var html = '';
+        if (!marker) {
+            // Just the icon
+            if (this._isSVGIcon()) {
+                html = '<div class="leaflet-vectormarker-singleicon">' + icon.html() + '</div>';
+            } else {
+                html = '<div class="leaflet-vectormarker-singleicon" style="color: ' + color + '"><i class="' + icon + '"></i></div>';
+            }
+        } else {
+            color = chroma(color);
+            var borderColor = chroma(color).alpha(0.5);
+            html = '<svg id="svg-vectormarker-marker" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 32 56">' +
+                      '<g id="svg-vectormarker">' +
+                        '<path id="svg_2" fill="' + color.css() + '" stroke="'+borderColor.css('rgba')+'" d="m15.64842,0c-8.58535,0 -15.64842,7.20061 -15.64842,16.11997c0,4.84818 1.00644,8.42613 2.6007,11.99846l13.04772,27.75208l13.04758,-27.75208c1.5941,-3.57232 2.60059,-7.15027 2.60059,-11.99846c0,-8.91936 -7.06302,-16.11997 -15.64818,-16.11997z"/>';
+            html += '</g>' +
+                    '</svg>';
+            if (icon) {
+                if (this._isSVGIcon()) {
+                    html += '<div class="leaflet-vectormarker-iconoverlay">'+icon.html()+'</div>';
+                } else {
+                    html += '<div class="leaflet-vectormarker-iconoverlay" style="color: #fff"><i class="' + icon + '"></i></div>';
+                }
+            }
+        }
+        return L.divIcon({
+            className: 'leaflet-vectormarker',
+            iconSize: L.point([25, 43]),
+            iconAnchor: L.point([12.5, 43.5]),
+            html: html
+        });
+    }
+});
