63be40b31f363bd153671b9d991743a3bb4a12f5
Jeremy Morel <jeremy.morel@opendatasoft.com>
2017-06-13 17:04:00 +0200

    update ods-widgets

---
+++ ods-widgets/ieshiv.js
@@ -0,0 +1,63 @@
+(function(exports) {
+    'use strict';
+
+    var customTags = [
+        'odsHighchartsChart',
+        'odsAggregation',
+        'odsAnalysis',
+        'odsCatalogContext',
+        'odsDatasetCard',
+        'odsMultidatasetsCard',
+        'odsDatasetContext',
+        'odsDisqus',
+        'odsDomainStatistics',
+        'odsFacetEnumerator',
+        'odsFacets',
+        'odsFacet',
+        'odsFacetCategoryList',
+        'odsFacetCategory',
+        'odsFilterSummary',
+        'odsGeotooltip',
+        'odsHighcharts',
+        'odsMultiHighcharts',
+        'odsChart',
+        'odsChartQuery',
+        'odsChartSerie',
+        'odsLastDatasetsFeed',
+        'odsLastReusesFeed',
+        'odsMapLegacy',
+        'odsMap',
+        'odsMapLayerGroup',
+        'odsMapLayer',
+        'odsMapTooltip',
+        'odsMostPopularDatasets',
+        'odsMostUsedThemes',
+        'odsPaginationBlock',
+        'odsPicto',
+        'odsThemePicto',
+        'odsMapPicto',
+        'odsResultEnumerator',
+        'odsResults',
+        'odsReuses',
+        'odsSearchbox',
+        'odsTable',
+        'odsTagCloud',
+        'odsTextSearch',
+        'odsThemeBoxes',
+        'odsTileMap',
+        'odsTimerange',
+        'odsTimescale',
+        'odsToggleModel',
+        'odsTopPublishers',
+        'odsTwitterTimeline',
+        'inject',
+        'odsFullClick'
+    ];
+    for (var i=0; i<customTags.length; i++) {
+        var elementName = customTags[i];
+        var elementTagName = elementName.replace(/([A-Z])/g, function (match) {
+          return "-" + match.toLowerCase();
+        });
+        document.createElement(elementTagName);
+    }
+}(window));
